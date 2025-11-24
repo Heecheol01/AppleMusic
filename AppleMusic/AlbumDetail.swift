ab6a11450f34f88f1b3125b9221e7fed8e6d28b9
Heecheol01 <khc3967@naver.com>
2025-11-24 11:19:31 +0900

    Home Done

--- AppleMusic/AlbumDetail.swift
+++ AppleMusic/AlbumDetail.swift
@@ -7,24 +7,74 @@
 
 import UIKit
 
-class AlbumDetail: UIViewController {
+class AlbumDetail: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var bad = ["Bad Dreams", "Bad Dreams"]
+    var hanroro = ["이상비행", "이상비행", "해초", "화해", "금붕어", "자처", "사랑하게 될 거야"]
+    var tour = ["I've Tried Everything But Therapy", "Some Things I'll Never Know", "Lose Control", "What More Can I Say", "The Door", "Goodbye's Been Good to You", "Last Communion", "You Still Get to Me", "SuitCase", "Flame", "Evergreen"]
+    var apartment = ["Apartment", "섬으로 가요 (Feat. 오혁)", "그저 그런 날", "Lost 2 (Feat.선우정아)", "Home Sweet Home", "444", "젊은 꿈", "Beyond (feat. O3ohn)", "갈게", "Blue Blue", "Mother", "Together"]
+    var jamong = ["자몽살구클럽", "내일에서 온 티켓", "용의자", "갈림길", "0+0", "__에게", "시간을 달리네", "도망"]
+    var harmony = ["harmony", "harmony", "네 번의 여름", "내일의 우리", "내겐 그 아무도 없을 거야", "Foolish World", "My Old Friend"]
     
     var collectionName: String?
     var titleText: String?
-    var color: UIColor?
+    var image: UIImage?
+    var singer: String?
+    var album: [String] = []
 
     @IBOutlet var lblCollection: UILabel!
     @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var lblSinger: UILabel!
     @IBOutlet var imgBackground: UIImageView!
+    @IBOutlet var tableListView: UITableView!
+    
+    // 1-based safe accessor for album array
+    private func albumTitle(atOneBased index: Int) -> String? {
+        // valid 1-based range is 1...album.count
+        guard index >= 1 && index <= album.count else { return nil }
+        return album[index - 1]
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         lblCollection.text = collectionName
         lblTitle.text = titleText
-        imgBackground.backgroundColor = color
+        lblSinger.text = singer
+        imgBackground.image = image
+        
+        tableListView.dataSource = self
+        tableListView.delegate = self
+        
+        if titleText == bad[0] {
+            album = bad
+        } else if titleText == hanroro[0] {
+            album = hanroro
+        } else if titleText == tour[0] {
+            album = tour
+        } else if titleText == apartment[0] {
+            album = apartment
+        } else if titleText == jamong[0] {
+            album = jamong
+        } else {
+            album = harmony
+        }
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return album.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Song", for: indexPath)
+        
+        let oneBasedIndex = indexPath.row + 1
+        if let title = albumTitle(atOneBased: oneBasedIndex) {
+            cell.textLabel?.text = "\(oneBasedIndex). \(title)"
+        } else {
+            cell.textLabel?.text = nil
+        }
+        
+        return cell
+    }
 }
-
--- AppleMusic/HomeViewController.swift
+++ AppleMusic/HomeViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 struct Album {
     let title: String
-    let color: UIColor
+    let singer: String
+    let image: UIImage?
 }
 
 class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
@@ -25,19 +26,33 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
     @IBOutlet var collectionView9: UICollectionView!
     @IBOutlet var collectionView10: UICollectionView!
     
+    // 각 컬렉션뷰마다 서로 다른 랜덤 6개를 유지
+    private var dataForCollection: [UICollectionView: [Album]] = [:]
+    
     // 더미 데이터
     private var albums: [Album] = [
-        Album(title: "Album 1", color: .systemPink),
-        Album(title: "Album 2", color: .systemBlue),
-        Album(title: "Album 3", color: .systemGreen),
-        Album(title: "Album 4", color: .systemOrange),
-        Album(title: "Album 5", color: .systemPurple),
-        Album(title: "Album 6", color: .systemTeal)
+        Album(title: "Bad Dreams", singer: "Teddy Swims", image: UIImage(named: "BadDreams.jpg")),
+        Album(title: "Apartment", singer: "Karthegarden", image: UIImage(named: "Apartment.jpg")),
+        Album(title: "I've Tried Everything But Therapy", singer: "Teddy Swims", image: UIImage(named: "LoseControl.jpg")),
+        Album(title: "이상비행", singer: "Hanroro", image: UIImage(named: "Flight.jpg")),
+        Album(title: "자몽살구클럼", singer: "Hanroro", image: UIImage(named: "Jamong.jpg")),
+        Album(title: "Harmony", singer: "Karthegarden", image: UIImage(named: "Harmony.jpg"))
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let all = albums
+        let carousels: [UICollectionView?] = [collectionView1, collectionView2, collectionView3, collectionView4,
+                                              collectionView5, collectionView6, collectionView7, collectionView8,
+                                              collectionView9, collectionView10]
+        
+        carousels.forEach { cvOpt in
+            if let cv = cvOpt {
+                dataForCollection[cv] = Array(all.shuffled().prefix(6))
+            }
+        }
         
         [collectionView1, collectionView5, collectionView7].forEach {
             cv in
@@ -82,22 +97,24 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
 
     // MARK: - UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        albums.count
+        return dataForCollection[collectionView]?.count ?? 0
     }
 
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let cell = collectionView.dequeueReusableCell(
+        let cell = collectionView.dequeueReusableCell(
             withReuseIdentifier: "인기 추천곡",
             for: indexPath
-        ) as? CollectionCell {
-            let album = albums[indexPath.item]
-            cell.imageView.backgroundColor = album.color
+        ) as! CollectionCell
+
+        if let album = dataForCollection[collectionView]?[indexPath.item] {
+            cell.imageView.image = album.image
             cell.titleLabel.text = album.title
-            return cell
         } else {
-            return UICollectionViewCell()
+            cell.imageView.image = nil
+            cell.titleLabel.text = nil
         }
+        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -123,10 +140,12 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
             else if collectionView === collectionView10 { collectionViewName = "Teddy Swims 팬이 즐겨 찾는 다른 항목" }
             else { collectionViewName = "unknown" }
 
-            let album = albums[indexPath.item]
-            dest.titleText = album.title
-            dest.collectionName = collectionViewName
-            dest.color = album.color
+            if let album = dataForCollection[collectionView]?[indexPath.item] {
+                dest.titleText = album.title
+                dest.collectionName = collectionViewName
+                dest.image = album.image
+                dest.singer = album.singer
+            }
         }
     }
 }
